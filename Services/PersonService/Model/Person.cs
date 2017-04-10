4b15b8d3b49a35d64f2054ead60bd22621b7e7f2
helto4real <tomas.hellstrom@yahoo.se>
2017-04-10 22:10:50 +0200

    Added person class

---
+++ Services/PersonService/Model/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonService.Model
+{
+    public class Person
+    {
+        public Guid Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
